99af763fddfc48bfcc50047a78baed628c536d49
RayzorArt <rayzor851@gmail.com>
2025-04-29 13:15:47 -0400

    Update ToolsSlideshow With Moving Function

--- client/src/ToolsSlideshow.js
+++ client/src/ToolsSlideshow.js
@@ -4,21 +4,21 @@ const images = [client/src/Human Centered Design Image, client/src/Worthwhile De
 let currentIndex = 0; //Current Index of image displayed
 function slideChange(goLeft) //Function to move left or right
 {
-  if (goLeft == true)
+  if (goLeft == true) //Go left
   {
     currentIndex -= 1;
   }
-  else
+  else //Go right
   {
     currentIndex += 1;
   }
-  if (currentIndex >= images.length)
+  if (currentIndex >= images.length) //Edge case. Loop around. 
   {
     currentIndex = 0;
   }
-  else if (currentIndex < 0)
+  else if (currentIndex < 0) //Edge case. Loop around
   {
     currentIndex = images.length - 1;
   }
-  document.getElementById().src = images[currentIndex];
+  document.getElementById().src = images[currentIndex]; //Obtain image from images[] to display
 }
